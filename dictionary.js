fcaac59bbe2d043d11be92527628e8ce248594f9
Fadeev Egor <fadeevegor@yandex.ru>
2023-04-25 22:49:12 +0300

    Test telegram lib integration

--- dictionary.js
+++ dictionary.js
@@ -11,8 +11,9 @@ let N_rounds = dictionary.length;
 
 function sampleOptions(dictionary, question, correctAnswer) {
     options = _.sample(dictionary, 4);
-    options = options.filter(option => option != [question, correctAnswer]);
-    options = options.map(option => option[1]).slice(0, 3);
+    options = options.filter(option => (option[0] != question) && (option[1] != correctAnswer));
+    options = options.map(option => option[1]);
+    options = options.slice(0, 3);
     options.push(correctAnswer);
     return _.shuffle(options);
 }
--- init.js
+++ init.js
@@ -48,23 +48,3 @@ function setCounter(counter, total) {
 function setMainButtonText(text) {
     telegram.MainButton.text = text;
 }
-
-
-dictionary = [
-    ["elma", "apple"],
-    ["ben", "i"],
-    ["biz", "we"],
-    ["sen", "you"],
-    ["siz", "вы"]
-];
-dictionary = _.shuffle(dictionary);
-let N_rounds = dictionary.length;
-
-
-function sampleOptions(dictionary, question, correctAnswer) {
-    options = _.sample(dictionary, 4);
-    options = options.filter(option => option != [question, correctAnswer]);
-    options = options.map(option => option[1]).slice(0, 3);
-    options.push(correctAnswer);
-    return _.shuffle(options);
-}
