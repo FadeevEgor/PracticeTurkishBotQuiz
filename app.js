5fa93a4e9e4f908acbd9f16f1f81e041126912cb
Fadeev Egor <fadeevegor@yandex.ru>
2023-04-25 16:39:44 +0300

    Test telegram lib integration

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
-tg.MainButton.text = "Finish!";
-tg.MainButton.show();
+let telegram = window.Telegram.WebApp;
+telegram.expand();
+telegram.MainButton.text = "Start!";
+telegram.MainButton.show();
+theme = telegram.themeParams;
 
-
-// text = document.createElement("h1")
-// text.innerHTML = "Hello, World!"
-// text.style = "color:white"
-// document.body.appendChild(text)
+button = document.createElement("button");
+button.innerHTML = "Click me!";
+button.style.backgroundColor = theme.button_color;
+document.body.appendChild(button);
