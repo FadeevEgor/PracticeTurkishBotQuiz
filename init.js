db63dbc3e52681d815b9fe32958c627b2a3f6cea
Fadeev Egor <fadeevegor@yandex.ru>
2023-04-27 02:55:16 +0300

    ...

--- init.js
+++ init.js
@@ -6,9 +6,9 @@ incorrect = "#FF3333";
 neutral = theme.button_color;
 
 telegram.expand();
-fullScreenHeight = telegram.viewportHeight;
-questionHeight = 150;
-counterHeight = 24;
-buttonTotalHeight = (fullScreenHeight - questionHeight - counterHeight) / 4;
-buttonHeight = round(0.9 * buttonTotalHeight);
-buttonUpperMargin = round(0.1 * buttonTotalHeight);
+// fullScreenHeight = telegram.viewportHeight;
+// questionHeight = 150;
+// counterHeight = 24;
+// buttonTotalHeight = (fullScreenHeight - questionHeight - counterHeight) / 4;
+// buttonHeight = round(0.9 * buttonTotalHeight);
+// buttonUpperMargin = round(0.1 * buttonTotalHeight);
