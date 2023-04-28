e655799cc91ff8cae495d9c42f1c2c05c4c0095c
Fadeev Egor <fadeevegor@yandex.ru>
2023-04-28 17:42:06 +0300

    ...

--- init.js
+++ init.js
@@ -17,9 +17,8 @@ if (PRODUCTION) {
     theme = telegram.themeParams;
     var enabledColor = new Color(theme.button_color);
     var backgroundColor = new Color(theme.bg_color);
-    var disabledColor = enabledColor.mix(backgroundColor, .5);
 } else {
     var enabledColor = new Color("#5288c1");
     var backgroundColor = new Color("white");
-    var disabledColor = correctColor.mix(enabledColor, .5);
 }
+var disabledColor = enabledColor.mix(backgroundColor, .5);
