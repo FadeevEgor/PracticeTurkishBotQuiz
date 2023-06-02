8fcd8db98ebe482a7f258a43e3c5ed9e4b25fb21
Fadeev Egor <fadeevegor@yandex.ru>
2023-06-02 13:29:23 +0300

    Fix defalut dictionary index value

--- src/init.js
+++ src/init.js
@@ -2,16 +2,13 @@
 var telegram = window.Telegram.WebApp;
 var PRODUCTION = !(telegram.platform == "unknown");
 var DEBUG = !PRODUCTION;
-
+const default_dictionary = 21;
 
 if (PRODUCTION) {
     telegram.expand();
     var url = new URL(window.location.href);
-    var index1 = parseInt(url.searchParams.get("tgWebAppStartParam"));
-    var index2 = parseInt(telegram.initDataUnsafe.start_param);
-    var index = index2;
 }
-index ??= 21;
+var index = parseInt(telegram.initDataUnsafe.start_param ?? default_dictionary);
 
 var progressBar = progressJs();
 progressBar.setOptions({ "theme": "blue", overlayMode: false });
